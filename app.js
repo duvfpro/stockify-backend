b52c572d900a9edad363afd5a1cd19d6c8f9b83d
duvfpro <148059791+duvfpro@users.noreply.github.com>
2024-08-11 15:52:24 +0200

    Update app.js
    Suite à CORS Fail

--- app.js
+++ app.js
@@ -3,19 +3,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+require('./models/connection');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
+var categoriesRouter = require('./routes/categories');
 
-var express = require('express');
 var app = express();
 const cors = require('cors');
 
-var corsOptions = {
-    origin: '*', // Permettre à toutes les origines
-    optionsSuccessStatus: 200 // Pour la compatibilité avec les anciens navigateurs
+// Définissez vos options CORS ici
+const corsOptions = {
+    origin: 'https://stockify-frontend-wine.vercel.app/', // Remplacez par l'URL de votre frontend déployé
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Restreignez aux méthodes HTTP que vous utilisez
+    credentials: true, // Si vous gérez des cookies ou des sessions
 };
 
+// Utilisez les options CORS
 app.use(cors(corsOptions));
+
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,5 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/categories', categoriesRouter);
 
 module.exports = app;
